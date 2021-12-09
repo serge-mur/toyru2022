a307f52a290202c130cb29166da35ff17aab180c
serge-mur <serge-mur@yandex.ru>
2021-12-09 15:43:56 +0300

    layout main page

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     const swiperMain = new Swiper(".swiper-main", {
         loop: true,
+        grabCursor: true,
         pagination: {
             el: '.swiper-pagination',
         },
@@ -12,15 +13,129 @@ document.addEventListener("DOMContentLoaded", function(event) {
     });
 
     const swiperAction = new Swiper(".swiper-action", {
-        slidesPerView: 4,
-        slidesPerGroup: 4,
-        spaceBetween: 30,
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 20,
         freeMode: true,
         autoHeight: true,
+        grabCursor: true,
         navigation: {
             nextEl: '.swiper-action-nav .swiper-outside-button-next',
             prevEl: '.swiper-action-nav .swiper-outside-button-prev',
         },
+        breakpoints: {
+            576: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+            },
+            992: {
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+            },
+        }
+    });
+
+    const swiperCharacters = new Swiper(".swiper-characters", {
+        slidesPerView: 2.5,
+        // slidesPerGroup: 2,
+        spaceBetween: 20,
+        freeMode: true,
+        autoHeight: true,
+        grabCursor: true,
+        navigation: {
+            nextEl: '.swiper-characters-nav .swiper-outside-button-next',
+            prevEl: '.swiper-characters-nav .swiper-outside-button-prev',
+        },
+        breakpoints: {
+            576: {
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+            },
+            992: {
+                slidesPerView: 5,
+                slidesPerGroup: 5,
+            },
+        }
+    });
+
+    const swiperCard = new Swiper(".swiper-card", {
+        slidesPerView: 2.5,
+        // slidesPerGroup: 2,
+        spaceBetween: 20,
+        freeMode: true,
+        autoHeight: true,
+        grabCursor: true,
+        navigation: {
+            nextEl: '.swiper-card1-nav .swiper-outside-button-next',
+            prevEl: '.swiper-card1-nav .swiper-outside-button-prev',
+        },
+        breakpoints: {
+            576: {
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+            },
+            992: {
+                slidesPerView: 5,
+                slidesPerGroup: 5,
+            },
+        }
+    });
+
+    const swiperReviews = new Swiper(".swiper-reviews", {
+        slidesPerView: 2.5,
+        slidesPerGroup: 2,
+        spaceBetween: 20,
+        freeMode: true,
+        autoHeight: true,
+        grabCursor: true,
+        navigation: {
+            nextEl: '.swiper-reviews-nav .swiper-outside-button-next',
+            prevEl: '.swiper-reviews-nav .swiper-outside-button-prev',
+        },
+        breakpoints: {
+            576: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+            },
+            992: {
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+            },
+        }
+    });
+
+    const swiperBrands = new Swiper(".swiper-brands", {
+        slidesPerView: 2.5,
+        slidesPerGroup: 1,
+        spaceBetween: 20,
+        freeMode: true,
+        grabCursor: true,
+        grid: {
+            fill: 'row',
+            rows: 3,
+        },
+        navigation: {
+            nextEl: '.swiper-brands-nav .swiper-outside-button-next',
+            prevEl: '.swiper-brands-nav .swiper-outside-button-prev',
+        },
+        breakpoints: {
+            576: {
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+                grid: {
+                    fill: 'row',
+                    rows: 2,
+                },
+            },
+            992: {
+                slidesPerView: 6,
+                slidesPerGroup: 6,
+                grid: {
+                    fill: 'row',
+                    rows: 2,
+                },
+            },
+        }
     });
 
 });
